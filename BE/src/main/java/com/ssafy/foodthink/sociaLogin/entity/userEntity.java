f99265bfc5f84fd2d23110785c0cc88c6aabe00d
김태영 <teayoung0812@naver.com>
2025-01-21 15:19:00 +0900

    feat: user 엔티티 구현

---
+++ BE/src/main/java/com/ssafy/foodthink/sociaLogin/entity/userEntity.java
@@ -0,0 +1,50 @@
+package com.ssafy.foodthink.sociaLogin.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class userEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(name="social_id")
+    private String socialId;
+
+    @Column(name = "social_type")
+    private String socialType;
+
+    private String email;
+
+    private String nickname;
+
+    private String image;
+
+    @Column(name="refresh_token")
+    private String refreshToken;
+
+    @Builder
+    public userEntity(String socialId, String socialType, String email) {
+        this.socialId = socialId;
+        this.socialType = socialType;
+        this.email = email;
+    }
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public void updateProfile(String nickname, String image) {
+        this.nickname = nickname;
+        this.image = image;
+    }
+
+}
