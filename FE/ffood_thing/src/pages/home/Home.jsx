15ef106ab957f5519ffb6412297e2bff0a245aff
정승국 <fightj@naver.com>
2025-01-24 17:37:34 +0900

    feat: 페이지테마 + 페이지슬라이드이펙트

--- FE/ffood_thing/src/pages/home/Home.jsx
+++ FE/ffood_thing/src/pages/home/Home.jsx
@@ -1,15 +1,17 @@
-// components/Home.js
 import React from "react"
 import SearchBar from "../../components/base/SearchBar"
 import HomeBigButton from "../../components/home/HomeBigButton"
 import "../../styles/home/Home.css"
+import PageSlide from "../../components/base/PageSlide"
 
 function Home() {
   return (
-    <div class="base-div">
-      <SearchBar />
-      <HomeBigButton />
-    </div>
+    <PageSlide>
+      <div className="base-div">
+        <SearchBar />
+        <HomeBigButton />
+      </div>
+    </PageSlide>
   )
 }
 
