4716edbd431e9554c4da84def3218a529ec77515
윤유빈 <ydbqls13@naver.com>
2025-01-31 19:23:03 +0900

    fix: Could not resolve placeholder 오류 조치

--- BE/src/main/java/com/ssafy/foodthink/global/S3Service.java
+++ BE/src/main/java/com/ssafy/foodthink/global/S3Service.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 @Service
-@PropertySource("classpath:config/application.properties")
+@PropertySource("classpath:application.properties")
 public class S3Service {
 
     private final S3Client s3Client;
--- BE/src/main/java/com/ssafy/foodthink/global/config/S3Config.java
+++ BE/src/main/java/com/ssafy/foodthink/global/config/S3Config.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 
 
 @Configuration
-@PropertySource("classpath:config/application.properties")
+@PropertySource("classpath:application.properties")
 //@EnableConfigurationProperties(AwsProperties.class)
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")
