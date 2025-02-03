df7bb09b0ef3650382d2eb07e494404ffa9c052d
김태영 <teayoung0812@naver.com>
2025-02-03 20:35:12 +0900

    feat: 리프레시 토큰으로 액세스 토큰 재발급 구현

--- BE/src/main/java/com/ssafy/foodthink/user/controller/AuthController.java
+++ BE/src/main/java/com/ssafy/foodthink/user/controller/AuthController.java
@@ -7,12 +7,15 @@ import com.ssafy.foodthink.user.service.CustomOAuth2UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -48,9 +51,37 @@ public class AuthController {
     @PostMapping("/logout")
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication(); // 현재 인증된 사용자의 정보를 가져옴
+
+        // 사용자의 인증 정보를 제거
         if (auth != null) {
-            SecurityContextHolder.clearContext();// 사용자의 인증 정보를 제거
+            SecurityContextHolder.clearContext();
         }
         return ResponseEntity.ok().body("사용자가 성공적으로 로그아웃되었습니다.");
+    }
+    
+    // 리프레시 토큰으로 액세스 토큰 재발급
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissueAccessToken(@RequestBody Map<String, String> tokenRequest) {
+        String refreshToken = tokenRequest.get("refreshToken");
+
+        // 리프레시 토큰 유효성 검사
+        if (!jwtUtil.validateToken(refreshToken)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 리프레시 토큰");
+        }
+
+        // DB에서 사용자 조회
+        String email = jwtUtil.getEmail(refreshToken);
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다"));
+
+        // 토큰 일치 여부 확인
+        if (!refreshToken.equals(user.getRefreshToken())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰 정보 불일치");
+        }
+
+        // 새로운 액세스 토큰 생성
+        String newAccessToken = jwtUtil.createAccessToken(user.getUserId(),user.getRole(),60 * 60 * 60L);
+
+        return ResponseEntity.ok().body(Map.of("accessToken", newAccessToken));
     }
 }
