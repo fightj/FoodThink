3ea78121f268b1dbcaf6fd19ad28237a21d17624
김태영 <teayoung0812@naver.com>
2025-02-03 10:48:34 +0900

    chore: 레시피 repository

--- BE/src/main/java/com/ssafy/foodthink/feed/tmp/RecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.foodthink.feed.tmp;
-
-import com.ssafy.foodthink.recipes.entity.RecipeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeRepository extends JpaRepository<RecipeEntity, Long> {
-}
