7842f0cf8bfb86d454c8b0a2eafce2a369ffc0c9
정승국 <fightj@naver.com>
2025-02-03 18:01:47 +0900

    feat: recipe-detail페이지작업중

--- FE/ffood_thing/src/pages/recipe/RecipeDetailPage.jsx
+++ FE/ffood_thing/src/pages/recipe/RecipeDetailPage.jsx
@@ -1,65 +1,128 @@
-import React from "react"
-import { useState } from "react"
-import { useParams } from "react-router-dom"
+import React, { useState } from "react"
+import { useParams, useNavigate } from "react-router-dom"
 import RecipeComponent from "../../components/recipe/RecipeComponent"
 import HandPoseComponent from "../../components/handmotion/HandPoseComponent" // HandPoseComponent 임포트
+import SearchBar from "../../components/base/SearchBar"
+import { Recipe } from "./recipe_data"
+import "../../styles/recipe/RecipeDetailPage.css"
 
 const RecipeDetailPage = () => {
   const { id } = useParams()
+  const navigate = useNavigate()
   const [showModal, setShowModal] = useState(false)
 
-  const recipeDetails = {
-    1: {
-      name: "Chocolate Cake",
-      steps: [
-        { pageNumber: 1, image: "path/to/image1.jpg", text: "Step 1: Gather ingredients." },
-        { pageNumber: 2, image: "path/to/image2.jpg", text: "Step 2: Mix ingredients." },
-      ],
-    },
-    2: {
-      name: "Spaghetti Carbonara",
-      steps: [
-        { pageNumber: 1, image: "path/to/image3.jpg", text: "Step 1: Boil pasta." },
-        { pageNumber: 2, image: "path/to/image4.jpg", text: "Step 2: Prepare sauce." },
-      ],
-    },
-    3: {
-      name: "Chicken Curry",
-      steps: [
-        { pageNumber: 1, image: "path/to/image5.jpg", text: "Step 1: Marinate chicken." },
-        { pageNumber: 2, image: "path/to/image6.jpg", text: "Step 2: Cook curry." },
-      ],
-    },
-  }
-
-  const recipe = recipeDetails[id]
+  const recipe = Recipe.find((item) => item.recipeId === parseInt(id))
 
   if (!recipe) {
-    return <div>Recipe not found.</div>
+    return <div>Recipe not found</div>
+  }
+
+  const getLevelText = (level) => {
+    switch (level) {
+      case 1:
+        return "하"
+      case 2:
+        return "중"
+      case 3:
+        return "상"
+      default:
+        return level
+    }
+  }
+
+  const scrollToSection = (section) => {
+    document.getElementById(section).scrollIntoView({ behavior: "smooth" })
   }
 
   return (
-    <div className="recipe-detail-page">
-      <h1>{recipe.name}</h1>
-      <button onClick={() => setShowModal(true)}>Start Cooking</button>
-      {showModal && (
-        <div className="modal-overlay">
-          <div className="modal-content">
-            <button className="close-button" onClick={() => setShowModal(false)}>
-              X
-            </button>
-            <HandPoseComponent
-              onNextPage={() => {
-                /* 다음 페이지로 이동하는 로직 구현 */
-              }}
-              onPrevPage={() => {
-                /* 이전 페이지로 이동하는 로직 구현 */
-              }}
-            />
-            <RecipeComponent pages={recipe.steps} />
+    <div className="base-div">
+      <SearchBar />
+      <div className="card-div-topsection">
+        <div style={{ width: "90%", margin: "0 auto" }}>
+          <button onClick={() => navigate(-1)} className="back-button">
+            <img src="/images/previous_button.png" alt="Previous" className="icon" />
+            이전
+          </button>
+
+          <div style={{ display: "flex", gap: "2rem", marginBottom: "100px" }}>
+            <div style={{ flex: "0 0 60%", position: "relative" }}>
+              <img src={recipe.image} alt="Recipe Image" className="recipe-image1" />
+              <img src={recipe.userImage} alt="프로필이미지" className="profile-image" />
+              <div className="nickname-container">
+                <h2 className="nickname-area">{recipe.nickname}</h2>
+                <button className="sub-btn">구독</button>
+              </div>
+            </div>
+
+            <div style={{ flex: "0 0 40%", display: "flex", flexDirection: "column", alignItems: "center" }}>
+              <div className="title-container">
+                <h1>{recipe.recipeTitle}</h1>
+              </div>
+              <div className="icon-container">
+                <div className="icon-item">
+                  <img src="/images/serving.png" alt="Serving" />
+                  <p>{recipe.serving}</p>
+                </div>
+                <div className="icon-item">
+                  <img src="/images/level.png" alt="Level" />
+                  <p>{getLevelText(recipe.level)}</p>
+                </div>
+                <div className="icon-item">
+                  <img src="/images/timerequired.png" alt="Time Required" />
+                  <p>{recipe.requiredTime}</p>
+                </div>
+              </div>
+              <button className="feed-btn" onClick={() => setShowModal(true)}>
+                Feed 작성
+              </button>
+              <button className="cook-btn" onClick={() => setShowModal(true)}>
+                조리시작
+              </button>
+            </div>
           </div>
+
+          {showModal && (
+            <div className="modal-overlay">
+              <div className="modal-content">
+                <button className="close-button" onClick={() => setShowModal(false)}>
+                  X
+                </button>
+                <HandPoseComponent
+                  onNextPage={() => {
+                    /* 다음 페이지로 이동하는 로직 구현 */
+                  }}
+                  onPrevPage={() => {
+                    /* 이전 페이지로 이동하는 로직 구현 */
+                  }}
+                />
+                <RecipeComponent pages={recipe.processes} />
+              </div>
+            </div>
+          )}
         </div>
-      )}
+      </div>
+
+      {/* Navigation Buttons */}
+      <div className="card-div-firstsection">
+        <button onClick={() => scrollToSection("ingredients")}>재료</button>
+        <button onClick={() => scrollToSection("steps")}>조리순서</button>
+        <button onClick={() => scrollToSection("completed")}>완성 이미지</button>
+        <button onClick={() => scrollToSection("feed")}>FEED</button>
+      </div>
+
+      {/* Sections */}
+      <div id="ingredients" className="card-div-section">
+        <h1>재료</h1>
+      </div>
+      <div id="steps" className="card-div-section">
+        <h1>조리 순서</h1>
+      </div>
+      <div id="completed" className="card-div-section">
+        <h1>완성 이미지</h1>
+      </div>
+      <div id="feed" className="card-div-section">
+        <h1>FEED</h1>
+      </div>
     </div>
   )
 }
---
+++ FE/ffood_thing/src/pages/recipe/recipe_data.js
@@ -0,0 +1,201 @@
+const Recipe = [
+  {
+    recipeId: 163,
+    recipeTitle: "바삭한 아란치니, 이탈리아 시칠리아 지방 전통음식",
+    nickname: "관리자",
+    userImage: "https://recipe1.ezmember.co.kr/cache/recipe/2015/09/17/ef602f6d4e33046cc613ca7ed1f50a76_m.jpg",
+    serving: "2인분",
+    level: 1,
+    requiredTime: "30분 이내",
+    image: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/43379879983e255fd8e0e8bccfe210af1.jpg",
+    ingredients: [
+      {
+        ingreName: "양파",
+        amount: "1/5개",
+      },
+      {
+        ingreName: "당근",
+        amount: "1/8개",
+      },
+      {
+        ingreName: "스팸",
+        amount: "1/5개",
+      },
+      {
+        ingreName: "밥",
+        amount: "1공기",
+      },
+      {
+        ingreName: "우유",
+        amount: "1/2컵",
+      },
+      {
+        ingreName: "체다치즈",
+        amount: "1장",
+      },
+      {
+        ingreName: "스틱 모짜렐라 치즈",
+        amount: "2개",
+      },
+      {
+        ingreName: "빵가루",
+        amount: "1/2밥공기",
+      },
+      {
+        ingreName: "밀가루",
+        amount: "1/3밥공기",
+      },
+      {
+        ingreName: "계란",
+        amount: "1개",
+      },
+    ],
+    processes: [
+      {
+        processOrder: 1,
+        processExplain: "양파, 당근, 스팸은 볶음밥용 크기로 잘게 잘라 준비합니다. 원래 아란치니는 생쌀과 버터, 화이트와인으로 만드는데 저는 지어놓은 밥으로 만들어볼게요.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/c210f39f35f0e003cd8e227cdf0400911.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 2,
+        processExplain: "달군 후라이팬에 기름을 두르고 양파, 당근, 스팸을 넣고 살짝 익을 때 까지 볶아줍니다.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/8db2848f339085617e301d0012e471f21.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 3,
+        processExplain: "밥 한공기 가득을 넣고 후추, 소금으로 간을 살짝 해주고 잘 볶아줍니다.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/6f1fa4de65adf3f10bdbc92dbb4981ad1.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 4,
+        processExplain: "밥과 잘 어우러지게 볶아지면 우유 1/2컵과 치즈 1장을 넣어요. 치즈를 넣는 이유는 점성을 이용해 동그랗게 잘 빚기 위함이에요.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/7c9b0b735b394813288ec02ad2cb4c891.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 5,
+        processExplain: "물기가 없고 잘 뭉쳐질 것 같으면 불을 끄고 식혀줍니다.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/877426a9c196374dc5eabf59b46cd8b51.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 6,
+        processExplain: "밥이 식으면 평평하게 만든 밥에 스틱형 모짜렐라 치즈 1/4를 넣고 뚜껑을 덮어 동그랗게 빚어줍니다! 스틱형 모짜렐라가 편한데 없으면 피자치즈를 이용해도 된답니다.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/d0e137458c13c0455a8b5b8b67d4cf261.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 7,
+        processExplain: "짜잔! 동그랗게 빚어 완성! 저는 6개로 만들었어요 :)",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/f4573bdb2624cce9e2dea39953d574371.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 8,
+        processExplain: "밀가를 묻혀줍니다.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/8dbf47b6a2277d13314ce76db72ff0791.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 9,
+        processExplain: "계란옷을 입혀주고.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/f34bd5d515a97abf719f4edb47a6fcb61.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 10,
+        processExplain: "튀김가루를 묻혀줍니다.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/cf636e569e34406c9a69ec7057bb67971.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 11,
+        processExplain: "튀김팬에 기름을 반 정도 붓고 굴리면서 튀겨도 예쁘게 튀겨진답니다 ^^",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/be34315284153f6d9dadbe9408bd09061.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 12,
+        processExplain: "노릇노릇 잘 튀겨진 아란치니!",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/6908721fa81834ecc49458bed2cba42a1.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 13,
+        processExplain: "이탈리아 음식과 어울리는 토마토소스를 접시에 깔고 위에 아란치니를 올립니다.",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/5105a17136afd9b016a7fd4838fa408a1.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 14,
+        processExplain: "먹음직스러운 이탈리아 시칠리아 전통음식, 아란치니가 완성되었답니다 :)",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/284c5bd6191c52df32c946496251d20a1.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 15,
+        processExplain: "바사삭 소리를 내며 반을 잘라보면 치즈가 들어있어요. 취향껏 치즈를 많이 넣으세요!",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/a713586bc05830143436bd0122a6efa21.jpg",
+          },
+        ],
+      },
+      {
+        processOrder: 16,
+        processExplain: "아란치니만 먹으면 싱거울 수 있으니 토마토소스와 함께 곁들이면 굿! 취향껏 크림파스타 소스도 괜찮을 것 같아요:)",
+        images: [
+          {
+            imageUrl: "https://recipe1.ezmember.co.kr/cache/recipe/2017/11/22/43379879983e255fd8e0e8bccfe210af1.jpg",
+          },
+        ],
+      },
+    ],
+  },
+]
+export { Recipe }
