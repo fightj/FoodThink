26f5081be56c98a31446f24d69b604e479e96d3f
김태영 <teayoung0812@naver.com>
2025-02-04 12:46:09 +0900

    feat: 사용자 TF-IDF 계산 구현중

--- BE/src/main/java/com/ssafy/foodthink/foodRecommend/service/UserTFIDFService.java
+++ BE/src/main/java/com/ssafy/foodthink/foodRecommend/service/UserTFIDFService.java
@@ -1,8 +1,11 @@
 package com.ssafy.foodthink.foodRecommend.service;
 
+import com.ssafy.foodthink.foodRecommend.entity.RecipeTfIdf;
 import com.ssafy.foodthink.foodRecommend.repository.RecipeTfIdfRepository;
 import com.ssafy.foodthink.foodRecommend.repository.RecommendInterestRepository;
+import com.ssafy.foodthink.recipeBookmark.entity.RecipeBookmarkEntity;
 import com.ssafy.foodthink.recipeBookmark.repository.RecipeBookmarkRepository;
+import com.ssafy.foodthink.recipes.entity.RecipeEntity;
 import com.ssafy.foodthink.user.entity.UserEntity;
 import com.ssafy.foodthink.user.entity.UserInterestEntity;
 import com.ssafy.foodthink.user.repository.UserRepository;
@@ -28,24 +31,24 @@ public class UserTFIDFService {
     private static final double DISLIKED_WEIGHT = -1.0;
     private static final double BOOKMARK_WEIGHT = 0.5;
 
-    public Map<String,Double> generateUserProfile(Long userId){
+    public Map<String,Double> generateUserProfile(Long userId) {
 
         Map<String, Double> userProfile = new HashMap<>();
 
         // 선호 재료
         List<String> likedIngredients = getLikedIngredients(userId);
-        for(String ingredient : likedIngredients){
+        for (String ingredient : likedIngredients) {
             userProfile.put(ingredient, LIKED_WEIGHT);
         }
 
         // 기피 재료
         List<String> dislikedIngredients = getDislikedIngredients(userId);
-        for(String ingredient : dislikedIngredients){
-            userProfile.put(ingredient,DISLIKED_WEIGHT);
+        for (String ingredient : dislikedIngredients) {
+            userProfile.put(ingredient, DISLIKED_WEIGHT);
         }
 
         // 북마크한 레시피의 IF-IDF 벡터 평균 계산
-
+        Map<String, Double> bookmarkProfile = calculateBookmarkProfile(userId);
 
 
         return null;
@@ -67,6 +70,25 @@ public class UserTFIDFService {
                 .stream()
                 .map(UserInterestEntity::getIngredient)
                 .collect(Collectors.toList());
+    }
+
+    private Map<String, Double> calculateBookmarkProfile(Long userId) {
+
+        List<RecipeBookmarkEntity> bookmarkedRecipes = recipeBookmarkRepository.findByUserEntity_UserId(userId);
+
+        Map<String, Double> aggregations = new HashMap<>(); // 각 특성 별 TF-IDF 합
+        Map<String, Integer> counts = new HashMap<>(); // 각 특성 별 TF-IDF 개수
+
+        for(RecipeBookmarkEntity recipe: bookmarkedRecipes){
+//            List<RecipeTfIdf> tfIdfValues = recipeTfIdfRepository.findByRecipe(recipe);
+//
+//            for
+        }
+
+
+
+
+        return null;
     }
 
 }
