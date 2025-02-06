b48559ea1aa007d3b410232faa1c9f3c1d9ee4cd
정승국 <fightj@naver.com>
2025-02-06 10:30:20 +0900

    fix: 로그인 통신

--- FE/ffood_thing/src/pages/login/LoginPage.jsx
+++ FE/ffood_thing/src/pages/login/LoginPage.jsx
@@ -2,64 +2,35 @@ import React, { useEffect } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import "../../styles/login/LoginPage.css"
 
-// async function fetchAccessToken(code) {
-//   try {
-//     const response = await fetch(`http://localhost:8080/api/auth/login/kakao?code=${code}`, {
-//       method: "GET",
-//       credentials: "include",
-//     })
-
-//     if (response.ok) {
-//       const accessToken = await response.text() // response.json() 대신 response.text() 사용
-//       return accessToken
-//     } else {
-//       console.error("Failed to fetch access token.")
-//       return null
-//     }
-//   } catch (error) {
-//     console.error("Error fetching access token:", error)
-//     return null
-//   }
-// }
-
 function LoginPage() {
   const location = useLocation()
   const navigate = useNavigate()
 
   useEffect(() => {
-    console.log("useEffect triggered")
+    // URLSearchParams를 이용하여 URL의 쿼리 파라미터를 가져옴
     const searchParams = new URLSearchParams(location.search)
-    const code = searchParams.get("code")
+    const accessToken = searchParams.get("accessToken")
 
-    console.log("Authorization code from URL:", code)
+    if (accessToken) {
+      // 액세스 토큰을 콘솔에 출력
+      console.log("Access token from URL:", accessToken)
 
-    if (code) {
-      console.log("Authorization code is present")
-      fetchAccessToken(code).then((accessToken) => {
-        if (accessToken) {
-          console.log("Access Token:", accessToken)
+      // 액세스 토큰을 로컬 스토리지에 저장
+      localStorage.setItem("accessToken", accessToken)
 
-          // 액세스 토큰을 로컬 스토리지에 저장
-          localStorage.setItem("accessToken", accessToken)
+      // 로그인 성공 메시지 출력
+      console.log("로그인 성공")
 
-          // 로그인 성공 메시지 출력
-          console.log("로그인 성공")
-
-          // 메인 페이지로 리디렉트
-          navigate("/")
-        } else {
-          console.log("Failed to retrieve access token")
-        }
-      })
+      // 메인 페이지로 리디렉트
+      navigate("/")
     } else {
-      console.log("Authorization code is not present")
+      console.log("Access token is not present")
     }
   }, [location, navigate])
 
   const handleKakaoClick = () => {
     console.log("Kakao login button clicked")
-    window.location.href =
-      "https://accounts.kakao.com/login/?continue=https%3A%2F%2Fkauth.kakao.com%2Foauth%2Fauthorize%3Fresponse_type%3Dcode%26client_id%3D71e6cc593f5444a36af1d3fe5dbb4f30%26redirect_uri%3Dhttp%253A%252F%252Fi12e107.p.ssafy.io%253A8085%252Foauth2%252Fauthorization%252Fkakao%26through_account%3Dtrue#login"
+    window.location.href = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=71e6cc593f5444a36af1d3fe5dbb4f30&redirect_uri=http://localhost:8080/oauth2/authorization/kakao"
   }
 
   return (
