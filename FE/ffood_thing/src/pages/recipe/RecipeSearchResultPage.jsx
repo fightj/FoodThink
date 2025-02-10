8024f95918828f0e30be3e3255a0f61a75db0870
PARKMINJE2 <alswp936@naver.com>
2025-02-10 17:58:42 +0900

    feat: elasticsearch

--- FE/ffood_thing/src/pages/recipe/RecipeSearchResultPage.jsx
+++ FE/ffood_thing/src/pages/recipe/RecipeSearchResultPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef, useCallback } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import axios from "axios"
 import SearchBarRecipe from "../../components/base/SearchBarRecipe"
@@ -8,80 +8,49 @@ function useQuery() {
   return new URLSearchParams(useLocation().search)
 }
 
-const FoodPagination = ({ currentPage, totalPages, onPageChange }) => {
-  const maxVisibleButtons = 10
-  let startPage = Math.floor(currentPage / maxVisibleButtons) * maxVisibleButtons
-
-  if (startPage + maxVisibleButtons > totalPages) {
-    startPage = Math.max(0, totalPages - maxVisibleButtons)
-  }
-
-  const foodText = Array.from({ length: maxVisibleButtons }, (_, index) => {
-    const pageIndex = startPage + index
-    return pageIndex < totalPages ? "o" : null
-  })
-
-  return (
-    <div className="pagination">
-      <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 0} className="navigation-button">
-        F
-      </button>
-      {foodText.map((char, index) => {
-        if (char === null) return null
-        const pageIndex = startPage + index
-        return (
-          <div key={index} className="page-button">
-            <button className={pageIndex === currentPage ? "active" : ""} onClick={() => onPageChange(pageIndex)}>
-              {char}
-            </button>
-            <div className="page-number">{pageIndex + 1}</div>
-          </div>
-        )
-      })}
-      <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages - 1} className="navigation-button">
-        d
-      </button>
-    </div>
-  )
-}
-
 const RecipeSearchResultPage = () => {
   const query = useQuery().get("query")
   const navigate = useNavigate()
   const [filteredRecipes, setFilteredRecipes] = useState([])
-  const [bookmarkedRecipes, setBookmarkedRecipes] = useState([])
   const [page, setPage] = useState(0)
-  const [size, setSize] = useState(20)
+  const [size, setSize] = useState(10)
   const [totalPages, setTotalPages] = useState(1)
-  const [totalResults, setTotalResults] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const observer = useRef()
+
+  const debounce = (func, delay) => {
+    let debounceTimer
+    return function (...args) {
+      const context = this
+      clearTimeout(debounceTimer)
+      debounceTimer = setTimeout(() => func.apply(context, args), delay)
+    }
+  }
+
+  const fetchRecipes = async (query, page, size) => {
+    setLoading(true)
+    try {
+      const params = {
+        query,
+        page,
+        size,
+        orderBy: "createdDate", // 기본값: 작성시간순
+      }
+
+      const response = await axios.get(`https://i12e107.p.ssafy.io/api/elasticsearch/search/pagenation`, { params })
+
+      setFilteredRecipes((prev) => (page === 0 ? response.data.content : [...prev, ...response.data.content]))
+      setTotalPages(response.data.totalPages)
+    } catch (error) {
+      console.error("Error fetching recipes", error)
+    } finally {
+      setLoading(false)
+    }
+  }
 
   useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const response = await axios.get(`https://i12e107.p.ssafy.io/api/elasticsearch/search/pagenation`, {
-          params: {
-            query,
-            page,
-            size,
-            orderBy: "createdDate", // 기본값: 작성시간순
-          },
-        })
-
-        setFilteredRecipes(response.data.content)
-        setTotalPages(response.data.totalPages)
-        setTotalResults(response.data.totalElements)
-
-        // 북마크된 레시피 로컬 스토리지에서 가져오기
-        const storedBookmarks = JSON.parse(localStorage.getItem("bookmarkedRecipes")) || []
-        setBookmarkedRecipes(storedBookmarks)
-      } catch (error) {
-        console.error("Error fetching recipes", error)
-        setFilteredRecipes([])
-      }
-    }
-
     if (query) {
-      fetchRecipes()
+      fetchRecipes(query, page, size)
     }
   }, [query, page, size])
 
@@ -89,26 +58,25 @@ const RecipeSearchResultPage = () => {
     navigate(`/recipes/${id}`)
   }
 
-  const handleSearch = (query) => {
-    navigate(`/search?query=${query}`)
-  }
+  const handleSearch = debounce((query) => {
+    setFilteredRecipes([])
+    setPage(0)
+    fetchRecipes(query, 0, size)
+  }, 300)
 
-  const handleBookmarkClick = (recipeId) => {
-    let updatedBookmarks = []
-    if (bookmarkedRecipes.includes(recipeId)) {
-      // 북마크 제거
-      updatedBookmarks = bookmarkedRecipes.filter((id) => id !== recipeId)
-    } else {
-      // 북마크 추가
-      updatedBookmarks = [...bookmarkedRecipes, recipeId]
-    }
-    setBookmarkedRecipes(updatedBookmarks)
-    localStorage.setItem("bookmarkedRecipes", JSON.stringify(updatedBookmarks)) // 로컬 스토리지에 저장
-  }
-
-  const handlePageChange = (newPage) => {
-    setPage(newPage)
-  }
+  const lastRecipeElementRef = useCallback(
+    (node) => {
+      if (loading) return
+      if (observer.current) observer.current.disconnect()
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && page < totalPages - 1) {
+          setPage((prevPage) => prevPage + 1)
+        }
+      })
+      if (node) observer.current.observe(node)
+    },
+    [loading, page, totalPages]
+  )
 
   return (
     <div className="base-div">
@@ -121,12 +89,17 @@ const RecipeSearchResultPage = () => {
               이전
             </button>
             <h3>
-              "{query}"에 대한 검색 결과가 {totalResults}개 있습니다.
+              "{query}"에 대한 검색 결과가 {filteredRecipes.length}개 있습니다.
             </h3>
           </div>
           <div className="recipe-list2">
-            {filteredRecipes.map((recipe) => (
-              <div key={recipe.recipeId} className="recipe-card2 recipe-card2-small" onClick={() => handleDetailClick(recipe.recipeId)}>
+            {filteredRecipes.map((recipe, index) => (
+              <div
+                key={recipe.recipeId}
+                ref={filteredRecipes.length === index + 1 ? lastRecipeElementRef : null}
+                className="recipe-card2 recipe-card2-small"
+                onClick={() => handleDetailClick(recipe.recipeId)}
+              >
                 <img src={recipe.image} alt={recipe.recipeTitle} className="recipe-image2" />
                 <div className="recipe-info2">
                   <h2>{recipe.recipeTitle}</h2>
@@ -137,9 +110,9 @@ const RecipeSearchResultPage = () => {
                 </div>
               </div>
             ))}
-            {filteredRecipes.length === 0 && <p>검색 결과가 없습니다.</p>}
+            {loading && <p>로딩 중...</p>}
+            {filteredRecipes.length === 0 && !loading && <p>검색 결과가 없습니다.</p>}
           </div>
-          <FoodPagination currentPage={page} totalPages={totalPages} onPageChange={handlePageChange} />
         </div>
       </div>
     </div>
