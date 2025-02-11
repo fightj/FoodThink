87eec43306d157b6f7b8df520c431ca84ad301fc
정승국 <fightj@naver.com>
2025-02-11 23:23:43 +0900

    feat: feed-write 북마크레시피 모달 스타일 개선

--- FE/ffood_thing/src/components/sns/UserBookmarkRecipe.jsx
+++ FE/ffood_thing/src/components/sns/UserBookmarkRecipe.jsx
@@ -1,10 +1,14 @@
-import React from "react"
-import Modal from "react-bootstrap/Modal"
-import Slider from "react-slick"
-import "bootstrap/dist/css/bootstrap.min.css"
-import Swal from "sweetalert2"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import React from "react";
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Swal from "sweetalert2";
+import "flickity/css/flickity.css"; // Flickity CSS 임포트
+import Flickity from "react-flickity-component"; // Flickity 컴포넌트 임포트
+import "../../styles/sns/UserBookmarkRecipe.css"; // 사용자 정의 CSS 임포트
+
+const flickityOptions = {
+  wrapAround: true
+};
 
 function UserBookmarkRecipe({ closeModal, bookmarks, onBookmarkSelect }) {
   const handleBookmarkClick = (bookmark) => {
@@ -15,77 +19,47 @@ function UserBookmarkRecipe({ closeModal, bookmarks, onBookmarkSelect }) {
       cancelButtonText: "아니오",
     }).then((result) => {
       if (result.isConfirmed) {
-        onBookmarkSelect(bookmark.recipeId)
-        closeModal()
+        onBookmarkSelect(bookmark.recipeId);
+        closeModal();
       }
-    })
-  }
-
-  const settings = {
-    infinite: true,
-    centerMode: true,
-    centerPadding: "0",
-    slidesToShow: 3,
-    speed: 500,
-    focusOnSelect: true,
-  }
+    });
+  };
 
   return (
     <Modal show onHide={closeModal} centered dialogClassName="modal-dialog" style={{ minWidth: "80vw", minHeight: "50vh", margin: "auto" }}>
       <Modal.Header closeButton>
         <Modal.Title>내 북마크 레시피</Modal.Title>
       </Modal.Header>
-      <Modal.Body
-        style={{
-          width: "100%",
-          height: "100%",
-          margin: "auto",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <Modal.Body style={{ width: "100%", height: "100%", margin: "auto", display: "flex", justifyContent: "center", alignItems: "center" }}>
         {bookmarks.length === 0 ? (
           <p>비어있습니다.</p>
         ) : (
-          <Slider {...settings} style={{ width: "100%" }}>
+          <Flickity
+            className={"bookmark-gallery"}
+            elementType={"div"}
+            options={flickityOptions}
+            disableImagesLoaded={false}
+            reloadOnUpdate
+          >
             {bookmarks.map((bookmark, idx) => (
               <div
                 key={idx}
-                className="bookmark-card"
+                className="bookmark-gallery-cell"
                 onClick={() => handleBookmarkClick(bookmark)}
-                style={{
-                  width: idx === 1 ? "50%" : "30%",
-                  height: idx === 1 ? "40vh" : "30vh",
-                  overflow: "hidden",
-                  position: "relative",
-                  cursor: "pointer",
-                  borderRadius: "10px",
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-                }}
+                style={{ cursor: "pointer", backgroundImage: `url(${bookmark.image})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                cursor: "pointer"}}
               >
-                <img src={bookmark.image} alt={bookmark.title} style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "10px" }} />
-                <div
-                  className="carousel-caption"
-                  style={{
-                    position: "absolute",
-                    bottom: "10px",
-                    left: "10px",
-                    color: "white",
-                    backgroundColor: "rgba(0, 0, 0, 0.5)",
-                    padding: "5px",
-                    borderRadius: "5px",
-                  }}
-                >
-                  <h5>{bookmark.title}</h5>
-                </div>
+                <h2>{bookmark.title}</h2>
+                <span className="scroll-item-date">{bookmark.date}</span>
               </div>
             ))}
-          </Slider>
+          </Flickity>
         )}
       </Modal.Body>
     </Modal>
-  )
+  );
 }
 
-export default UserBookmarkRecipe
+export default UserBookmarkRecipe;
