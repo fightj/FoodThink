88db1b47f2f539bf0754d179d30ac9133d1f08ed
PARKMINJE2 <alswp936@naver.com>
2025-02-14 10:29:02 +0900

    fix: recipe-searchbar

--- FE/ffood_thing/src/pages/recipe/DemoCookingPage.jsx
+++ FE/ffood_thing/src/pages/recipe/DemoCookingPage.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { useLocation } from "react-router-dom"
 import "../../styles/recipe/DemoCookingPage.css" // 스타일을 위한 CSS 파일을 임포트합니다.
 
-import RecipeInfo from "../../components/recipe/Welcome"
+import Welcome from "../../components/recipe/Welcome"
 
 const DemoCookingPage = () => {
   const location = useLocation()
@@ -10,7 +10,7 @@ const DemoCookingPage = () => {
 
   return (
     <div className="cooking-container">
-      <RecipeInfo recipe={recipe} />
+      <Welcome recipe={recipe} />
     </div>
   )
 }
