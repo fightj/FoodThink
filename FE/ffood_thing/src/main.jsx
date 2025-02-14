7c961c7db3f39fa026b4daa8ef62b56ab0f813ad
Kang hye kyoung <ghgghg96@naver.com>
2025-02-14 17:33:57 +0900

    style: div-container

--- FE/ffood_thing/src/main.jsx
+++ FE/ffood_thing/src/main.jsx
@@ -1,11 +1,7 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // 두번 렌더링
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
