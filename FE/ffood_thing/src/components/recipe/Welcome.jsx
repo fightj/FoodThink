13e8e2b70e004a71b0dd591149a8c31a1076e501
PARKMINJE2 <alswp936@naver.com>
2025-02-17 09:38:42 +0900

    to merge

--- FE/ffood_thing/src/components/recipe/Welcome.jsx
+++ FE/ffood_thing/src/components/recipe/Welcome.jsx
@@ -12,11 +12,7 @@ const Welcome = ({ recipe, onNextPage, onPrevPage }) => {
       <div className="ready-for-cooking">
         <p>요리를 시작하려면 화면을 터치하세요.</p>
       </div>
-      {/* <button className="start-button" onClick={onNextPage}>
-        요리 시작
-      </button> */}
-      {/* <img className="welcome-image-knife" src="/images/knife.png" alt="knife" />
-      <img className="welcome-image-shaker" src="/images/bubblemaker.png" alt="bubblemaker" /> */}
+
     </div>
   )
 }
